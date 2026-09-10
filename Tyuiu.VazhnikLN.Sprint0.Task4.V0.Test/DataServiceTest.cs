60ba25ac986c50d4ca6cac9805e0ec4e5310ad62
Лука Важник <Tyuiu.VazhnikLN@outlook.com>
2026-09-10 12:51:57 +0500

    Task4

---
+++ Tyuiu.VazhnikLN.Sprint0.Task4.V0.Test/DataServiceTest.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.VazhnikLN.Sprint0.Task4.V0.Lib; 
+
+namespace Tyuiu.VazhnikLN.Sprint0.Task4.V0.Test
+
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionValid()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckSubstractionValid()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        [TestMethod]
+
+        public void CheckMultiplicationValid()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+
+        public void CheckDivisionValid()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
