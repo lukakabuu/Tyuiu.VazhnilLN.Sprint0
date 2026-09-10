f2168b4a42e34ab02b8ba4ef92d93cbef7edaa4a
Лука Важник <Tyuiu.VazhnikLN@outlook.com>
2026-09-10 10:28:32 +0500

    Task2

---
+++ Tyuiu.VazhnikLN.Sprint0.Task2.V0.Test/DataserviceTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Tyuiu.VazhnikLN.Sprint0.Task2.V0.Lib;
+
+
+namespace Tyuiu.VazhnikLN.Sprint0.Task2.V0.Test
+{
+    [TestClass]
+    public sealed class DataserviceTest
+    {
+        [TestMethod]
+        public void CheckGetMessageValid()
+        {
+            var name = "Лука";
+            var res = DataService.GetMessage(name);
+            Assert.AreEqual("Привет, Лука", res);
+        }
+    }
+}
---
+++ Tyuiu.VazhnikLN.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.VazhnikLN.Sprint0.Task2.V0.Lib;
+
+namespace Tyuiu.VazhnikLN.Sprint0.Task1.V0;
+class Programm
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Лука"));
+        Console.ReadKey();
+        Console.ReadKey();
+
+    }
+
+}
